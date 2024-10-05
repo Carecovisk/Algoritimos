c4fd9cc80eec977b6086db9692b45b157279f96c
João Victor Ribeiro Santos <joaovictoribeirosantos11@gmail.com>
2024-10-05 16:36:41 -0300

    first commit

---
+++ heapSort.py
@@ -0,0 +1,34 @@
+def heapify(arr, n, i):
+    parent = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    # Se o filho na esquerda é maior
+    if left < n and arr[left] > arr[parent]:
+        parent = left
+
+    # Se o filho na direita é maior
+    if right < n and arr[right] > arr[parent]:
+        parent = right
+
+    if parent != i:
+        arr[i], arr[parent] = arr[parent], arr[i]  # Troca
+        heapify(arr, n, parent)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    # Constrói o Max Heap
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n-1, 0, -1):
+        # Move a raiz atual para o final
+        arr[i], arr[0] = arr[0], arr[i]
+
+        heapify(arr, i, 0)
+
+arr = [3, 5, 1, 2, 7, 8, 10, 4, 6, 9]
+print("Array antes: " + arr.__str__())
+heap_sort(arr)
+print("Array depois: " + arr.__str__())
